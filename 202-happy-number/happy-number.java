7124247b0cc87761598a0dcb493fa297d21c7aaa
dhruvwadyalkar-svg <dhruvwadyalkar@gmail.com>
2026-09-22 23:33:50 +0530

    Time: 1 ms (77.89%) | Memory: 42.2 MB (88.39%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isHappy(int n) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        while(true){
+            int sum =0;
+            while(n>0){
+                int dig=n%10;
+                sum+=dig*dig;
+                n/=10;
+            }
+            if(sum == 1) return true;
+            if(list.contains(sum)) return false;
+            else list.add(sum);
+            n=sum;
+        }
+    }
+}
